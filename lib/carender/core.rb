f828c34eeefb094ac47adfaa4b5884083d78f33a
kami-zh <hiroki.zenigami@gmail.com>
2018-01-19 02:02:41 +0900

    Refactor

--- lib/carender/core.rb
+++ lib/carender/core.rb
@@ -7,13 +7,21 @@ module Carender
     end
 
     def render(&block)
-      calendar = Carender::Calendar.new(*view_context.params.values_at(:year, :month).map(&:to_i))
+      calendar = Carender::Calendar.new(year, month)
       calendar.render(view_context, grouped_collection, &block).html_safe
     end
 
     private
 
     attr_reader :view_context, :collection, :column
+
+    def year
+      view_context.params[:year].to_i
+    end
+
+    def month
+      view_context.params[:month].to_i
+    end
 
     def grouped_collection
       Hash.new([]).tap do |hash|
