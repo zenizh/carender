2f8cf378b3b701f6a6665c2e3ad72d84229390c9
kami-zh <hiroki.zenigami@gmail.com>
2018-01-19 18:40:35 +0900

    Add test

--- carender.gemspec
+++ carender.gemspec
@@ -7,14 +7,14 @@ Gem::Specification.new do |s|
   s.version     = Carender::VERSION
   s.authors     = ['kami-zh']
   s.email       = ['hiroki.zenigami@gmail.com']
-  s.homepage    = 'https://github.com/kami-zh/carender'
   s.summary     = 'A simple calendar gem for Rails application.'
   s.description = 'A simple calendar gem for Rails application.'
+  s.homepage    = 'https://github.com/kami-zh/carender'
   s.license     = 'MIT'
+  s.files       = Dir['lib/**/*', 'LICENSE.txt', 'Rakefile', 'README.md']
 
-  s.files = Dir['{app,config,db,lib}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.md']
+  s.add_dependency 'activesupport'
 
-  s.add_dependency 'rails', '~> 5.2.0.beta2'
-
-  s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'actionview'
+  s.add_development_dependency 'oga'
 end
--- test/carender_test.rb
+++ test/carender_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
+require 'action_view'
+require 'oga'
 
-class Carender::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Carender
+class CarenderTest < Minitest::Test
+  module ViewContext
+    include ActionView::Context
+    include ActionView::Helpers::CaptureHelper
+
+    attr_accessor :params
+  end
+
+  include Carender::Helper
+  include ViewContext
+
+  def setup
+    @params = { year: 2018, month: 1 }
+  end
+
+  def test_carender
+    html = carender do |date|
+      date.to_s
+    end
+    document = Oga.parse_html(html)
+    assert_equal 'Sun', document.xpath('table/tr[1]/th[1]').text
+    assert_equal 'Sat', document.xpath('table/tr[1]/th[7]').text
+    assert_empty document.xpath('table/tr[2]/td[1]').text
+    assert_empty document.xpath('table/tr[6]/td[5]').text
+    assert_equal '2018-01-01', document.xpath('table/tr[2]/td[2]').text
+  end
+
+  def test_carender_with_no_block
+    html = carender
+    document = Oga.parse_html(html)
+    assert_empty document.xpath('table/tr[2]/td[2]').text
+  end
+
+  def test_carender_with_collection
+    post = Struct.new(:title, :posted_on)
+
+    posts = [
+      post.new('Post #1', Date.new(2018, 1, 1)),
+      post.new('Post #2', Date.new(2018, 1, 1)),
+      post.new('Post #3', Date.new(2018, 1, 2))
+    ]
+
+    html = carender collection: posts, column: :posted_on do |_, posts|
+      html = ''
+      posts.each do |post|
+        html << post.title
+      end
+      html
+    end
+
+    document = Oga.parse_html(html)
+    assert_match /Post #1/, document.xpath('table/tr[2]/td[2]').text
+    assert_match /Post #2/, document.xpath('table/tr[2]/td[2]').text
+    assert_match /Post #3/, document.xpath('table/tr[2]/td[3]').text
   end
 end
